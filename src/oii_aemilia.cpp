2cc3cac14ea73e375870e6e769bb9631d4ef1523
altomezzatacca <taccagianmario@gmail.com>
2026-08-15 01:31:52 +0200

    .

--- src/oii_aemilia.cpp
+++ src/oii_aemilia.cpp
@@ -1,43 +1,31 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
 vector<int> verifica(int N, vector<int> T) {
-	vector<int> V;
-	deque<pair<int, int>> D;
-	for (int i=0; i<N; i++){
-		D.push_back({T[i], i});
-	}
-	sort(D.begin(), D.end());
-	V.push_back(D[0].second);
-	int r=D[0].second, l=D[0].second;
-	int t=0;
-	D.pop_front();
-	bool possible=1;
-	while(!D.empty() && possible){
-		if(D[0].second>=l && D[0].second<=r) {
-			D.pop_front();
-			continue;
-		}
-		if(D[0].second>r){
-			while(r<D[0].second && t<D[0].first){
-				t++; r++;
-				if (r!=D[0].second) V.push_back(r);
-			}
-			if(D[0].second==r){
-				V.push_back(D[0].second);
-				D.pop_front();
-			} else possible=0;
-		} else if(D[0].second<l){
-			while(l>D[0].second && t<D[0].first){
-				t++; l--;
-				if (l!=D[0].second) V.push_back(l);
-			}
-			if(D[0].second==l){
-				V.push_back(D[0].second);
-				D.pop_front();
-			} else possible=0;
-		}
-	}
-	if (!possible) return{};
-	else return V;
+    int &n = N;
+    vector<int> ans;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+    for (int i = 0; i < n; i++)
+        q.emplace(T[i], i);
+    int l, r = l = q.top().second;
+    int t = 0;
+    ans.emplace_back(l);
+    while (!q.empty()) {
+        auto [tt, u] = q.top(); q.pop();
+        if (u >= l && u <= r) continue;
+        if (u > r) {
+            if(t + u - r > tt) return {};
+            for(int i = r + 1; i <= u; i++)
+                ans.emplace_back(i);
+            t += u - r;
+            r = u;
+        } else if (u < l) {
+            if(t + l - u > tt) return {};
+            for(int i = l - 1; i >= u; i--)
+                ans.emplace_back(i);
+            t += l - u;
+            l = u;
+        }
+    }
+    return ans;
 }
