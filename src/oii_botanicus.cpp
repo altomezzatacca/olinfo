edff8452c62d03f1627ff0ef899e8e153fbd4d88
altomezzatacca <taccagianmario@gmail.com>
2026-08-15 02:24:13 +0200

    .

--- src/oii_botanicus.cpp
+++ src/oii_botanicus.cpp
@@ -1,88 +1,33 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-const int MAXN=1500000;
-
-void dfs(vector<vector<int>> &adj, vector<int> &H, vector<int> &max_H, int u){
-	max_H[u]=H[u];
-	for(auto v: adj[u]){
-		H[v]=H[u]+1;
-		dfs(adj, H, max_H, v);
-		max_H[u]=max(max_H[u], max_H[v]);
-	}
-}
-
-int pota(int N, vector<int> P){
-	
-	int ans=0;
-	vector<vector<int>> adj(N);
-	for(int i=1; i<N; i++) adj[P[i]].push_back(i);
-	
-	vector<int> H(N), max_H(N);
-	H[0]=0;
-	
-	dfs(adj, H, max_H, 0);
-	
-	int maxH = *max_element(max_H.begin(), max_H.end());
-	vector<int> sweep(maxH+2, 0);
-	
-	for(int i=0; i<N; i++){
-		sweep[H[i]]++;
-		sweep[max_H[i]+1]--;
-	}
-	
-	int val=0;
-	for(int i=0; i<=maxH; i++){
-		val+=sweep[i];
-		ans=max(ans, val);
-	}
-	
-	return N-ans;
-} 
-
-/*
-struct nodo{
-	int padre;
-	int h;
-	bool foglia=1;
-	set<int> al;
-	set<int> son;
-};
-
-void alinsert(vector<nodo> &V, int ix, int h){
-	V[ix].al.insert(h);
-	V[ix].son.insert(ix);
-	if(ix!=0) alinsert(V, V[ix].padre, h);
-}
-
-bool comp(nodo &a, nodo &b){
-	return a.h<b.h;
-}
-
-bool comp2(nodo &a, int x){
-	return a.h<x;
-}
+// quando checko l'altezza x, devo tagliare tutti e soli i nodi con:
+// h > x
+// max_h (nel sottoalbero) < x
 
 int pota(int N, vector<int> P) {
-    vector<nodo> V(N);
-    V[0]={-1, 0, 0, {0}, {}};
-    for(int i=1; i<N; i++){
-    	V[i].padre=P[i];
-    	V[i].h=V[P[i]].h+1;
-    	V[i].al.insert(V[i].h);
-    	alinsert(V, V[i].padre, V[i].h);
-	}
-	for(int i=1; i<N; i++) V[P[i]].foglia=0;
-	sort(V.begin(), V.end(), comp);
-	int mini=MAXN, maxH=V[N-1].h;
-	vector<nodo> V1;
-	for(int i=0; i<=maxH; i++){
-		V1=V;
-		int x=0;
-		int h=(*lower_bound(V.begin(), V.end(), i, comp2)).h;
-		for(int i=1; i<N; i++){
-			if(V[])
-		}
-		mini=min(mini, x);
-	}
-	return x;
-}*/ // ma wtf stavo facendo, lo terrò come ricordo
+    int &n = N;
+    vector<int> H(n), max_H(n, -1);
+    vector<vector<int>> adj(n);
+    for (int i = 1; i < n; i++)
+        adj[P[i]].emplace_back(i);
+    function <void(int)> dfs = [&] (int node) -> void {
+        max_H[node] = H[node];
+        for(auto u: adj[node]){
+            H[u] = H[node] + 1;
+            dfs(u);
+            max_H[node] = max(max_H[node], max_H[u]);
+        }
+    };
+    dfs(0);
+    vector<int> V(n); // sweep line
+    for (int i = 0; i < n; i++) {
+        V[H[i]]++; // da qua in poi ho un nodo valido in più
+        V[max_H[i] + 1]--; // dal +1 in poi ho un nodo valido in meno
+    }
+    int ans = 0, cur = 0; // nodi che posso tenere
+    for (int h = 0; h < n; h++) {
+        cur += V[h];
+        ans = max(ans, cur); // massimizzo il numero di nodi che posso tenere
+    }
+    return N - ans;
+}
