73b091c6c2aec17277ef3ebf4d28ef19252a1373
altomezzatacca <taccagianmario@gmail.com>
2026-08-31 17:22:14 +0200

    .

--- src/oii_candele.cpp
+++ src/oii_candele.cpp
@@ -1,75 +1,61 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define ff first
-#define ss second
-#define endl "\n"
+#include "bits/stdc++.h"
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-constexpr static int inf = 2e9;
-constexpr static ll linf = 1e18;
-constexpr static int mod1 = 1e9 + 7;
-constexpr static int mod2 = 998244353;
-
-struct Event{
-	int ix, type, i;
-};
-
-enum { Start, End };
+using ll = long long;
+constexpr static ll inf = 1e17;
 
 void brucia(int N, vector<int> &M, vector<int> &B, vector<long long> &T) {
-	vector<Event> events;
-	vector<vector<pii>> adj(N);
-	for(int i = 0; i < N; i++){
-		events.pb({M[i], Start, i});
-		events.pb({B[i], End, i});
-	}
-	
-	sort(events.begin(), events.end(), [](Event a, Event b){
-		return make_tuple(a.ix, a.type, a.i) < make_tuple(b.ix, b.type, b.i);
-	});
-	set<pii> active;
-	for(auto [ix, type, i]: events){
-		if(type == Start){
-			if(!active.empty()){
-				auto [x, idx] = *active.rbegin();
-				adj[idx].pb({i, abs(ix - x)});
-			}
-			if(B[i] > M[i])
-				active.insert({ix, i});
-		} else{
-			active.erase({M[i], i});
-		}
-	}
-	
-	sort(events.begin(), events.end(), [](Event a, Event b){
-		return make_tuple(-a.ix, a.type, a.i) < make_tuple(-b.ix, b.type, b.i);
-	});
-	for(auto [ix, type, i]: events){
-		if(type == Start){
-			if(!active.empty()){
-				auto [x, idx] = *active.begin();
-				adj[idx].pb({i, abs(ix - x)});
-			}
-			if(B[i] < M[i])
-				active.insert({ix, i});
-		} else{
-			active.erase({M[i], i});
-		}
-	}
-
-	vector<bool> visited(N);
-	priority_queue<pll, vector<pll>, greater<pll>> pq;
-	for(int i = 0; i < N; i++) T[i] = linf;
-	pq.push({T[0] = 0, 0});
-	
-	while(!pq.empty()){
-		auto [t, node] = pq.top(); pq.pop();
-		if(visited[node]) continue; visited[node] = true;
-		for(auto [x, w]: adj[node])
-			if(T[x] > t + w)
-				pq.push({T[x] = t + w, x});
-	}
-	for(auto &x: T) if(x == linf) x = -1;
+    int &n = N;
+    vector<array<int, 3>> sweep;
+    vector<vector<pair<int, int>>> adj(n);
+    for (int i = 0; i < n; i++)
+        sweep.push_back({M[i], 0, i}), // accensione
+        sweep.push_back({B[i], 1, i}); // fine
+    auto comp1 = [&] (array<int, 3> a, array<int, 3> b) -> bool {
+        return make_tuple(a[0], a[1], a[2]) < make_tuple(b[0], b[1], b[2]);
+    };
+    auto comp2 = [&] (array<int, 3> a, array<int, 3> b) -> bool {
+        return make_tuple(-a[0], a[1], a[2]) < make_tuple(-b[0], b[1], b[2]);
+    };
+    sort(begin(sweep), end(sweep), comp1);
+    set<pair<int, int>> active;
+    for (auto [pos, type, i]: sweep) {
+        if (type == 0) {
+            if (!active.empty()) {
+                auto [tmp, ix] = *active.rbegin();
+                adj[ix].emplace_back(i, abs(M[i] - M[ix]));
+            }
+            if (B[i] > M[i])
+                active.insert({pos, i});
+        } else {
+            active.erase({M[i], i});
+        }
+    }
+    active.clear();
+    sort(begin(sweep), end(sweep), comp2);
+    for (auto [pos, type, i]: sweep) {
+        if (type == 0) {
+            if (!active.empty()) {
+                auto [tmp, ix] = *active.begin();
+                adj[ix].emplace_back(i, abs(M[i] - M[ix]));
+            }
+            if (B[i] < M[i])
+                active.insert({pos, i});
+        } else {
+            active.erase({M[i], i});
+        }
+    }
+    active.clear();
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+    for (auto &u: T) u = inf;
+    pq.emplace(T[0] = 0, 0);
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop();
+        if (d > T[u]) continue;
+        for (auto [v, w]: adj[u])
+            if (T[v] > T[u] + w)
+                pq.emplace(T[v] = T[u] + w, v);
+    }
+    for (auto &u: T)
+        if (u == inf)
+            u = -1;
 }
